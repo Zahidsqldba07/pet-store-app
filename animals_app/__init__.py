57c60bdb229927328092e769d5ff9fcd4aa4d3cd
Saima Yunus <saimayunus925@gmail.com>
2021-07-30 19:09:51 -0700

    configured DB location, deleted views.py for now, etc

--- animals_app/__init__.py
+++ animals_app/__init__.py
@@ -1,3 +1,18 @@
 
 # basically just initialize our app and DB here
 
+from flask import Flask, render_template # importing Flask obj to make app and 'render_template' module to render HTML templates
+
+animals_app = Flask(__name__) # initializing our app obj
+
+from dotenv import load_dotenv # importing 'load_dotenv()' to load our env variables
+import os # importing Python library for interfacing with OS
+load_dotenv()
+
+animals_app.config["MONGO_URI"] = os.getenv("MONGO_URI") # configuring our DB location
+
+@animals_app.route('/')
+@animals_app.route('/index')
+def index():
+    return "This is a homepage!"
+
---
+++ run.py
@@ -0,0 +1,6 @@
+
+from animals_app import animals_app # importing our app obj from app pkg
+
+if __name__ == '__main__':
+    animals_app.run(debug=True) # if __name__ = current module (run.py), we run the app on local server with debug = true
+    # this makes sure server restarts upon code changes
