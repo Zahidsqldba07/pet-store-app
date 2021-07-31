910425074cffdc4c636e663e122725e8c71ff9cf
Saima Yunus <saimayunus925@gmail.com>
2021-07-30 19:30:26 -0700

    made index template, put everything in __init__ file for now

--- animals_app/__init__.py
+++ animals_app/__init__.py
@@ -2,14 +2,14 @@
 # basically just initialize our app and DB here
 
 from flask import Flask, render_template # importing Flask obj to make app and 'render_template' module to render HTML templates
-
-animals_app = Flask(__name__) # initializing our app obj
-
+from flask_pymongo import PyMongo # importing PyMongo() obj for interfacing with our MongoDB database
 from dotenv import load_dotenv # importing 'load_dotenv()' to load our env variables
 import os # importing Python library for interfacing with OS
 load_dotenv()
 
+animals_app = Flask(__name__) # initializing our app obj
 animals_app.config["MONGO_URI"] = os.getenv("MONGO_URI") # configuring our DB location
+animals_db = PyMongo(animals_app) # instantiating our DB obj for use with our app
 
 @animals_app.route('/')
 @animals_app.route('/index')
--- animals_app/mongo.py
+++
@@ -1,5 +0,0 @@
-
-from . import animals_app # importing our app obj from our app pkg
-from flask_pymongo import PyMongo # importing PyMongo() obj for interfacing with our MongoDB database
-
-animals_db = PyMongo(animals_app) # instantiating our DB obj for use with our app
